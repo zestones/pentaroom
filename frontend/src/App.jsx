746d9e4c13863d40d157084f6d433bac651b1721
Antoine <antoinechataignier1998@orange.fr>
2022-02-20 16:50:21 +0100

    feat(ci): test env variable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css'
-import Canvas from './components/canvas/Canvas'
 
 function App() {
   return (
-    <Canvas />
+    <p>
+      {process.env.REACT_APP_ENDPOINT}
+      {' '}
+      - Test
+    </p>
   )
 }
 

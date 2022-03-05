c147bc5e7ccf47339dff463d3fecbdbb26f95161
Antoine <antoinechataignier1998@orange.fr>
2022-03-05 14:39:16 +0100

    feat: add switch role button

---
+++ frontend/src/components/switchRoleButton/SwitchRoleButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+  button: {
+    position: 'absolute',
+    bottom: '30px',
+    left: '30px',
+  },
+})
+
+function SwitchRoleButton({ isDrawer, setIsDrawer }) {
+  const classes = useStyles()
+
+  const handleClick = () => {
+    setIsDrawer(!isDrawer)
+  }
+  return (
+    <Button variant="contained" className={classes.button} onClick={handleClick}>{isDrawer ? 'Passer spectateur' : 'Passer dessinateur'}</Button>
+  )
+}
+
+export default SwitchRoleButton

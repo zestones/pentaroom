fe9bf32e6a78bfe27f7ed1c880a24a6cff0fdd9c
Idriss <idrissbenguezzou@gmail.com>
2022-03-11 18:58:11 +0100

    refactor(Canvas): split file -> fill function in fillCanvas file

--- frontend/src/components/canvas/Canvas.jsx
+++ frontend/src/components/canvas/Canvas.jsx
@@ -1,11 +1,7 @@
-/* eslint-disable no-unreachable-loop */
-/* eslint-disable no-unreachable */
-/* eslint-disable no-param-reassign */
-/* eslint-disable prefer-destructuring */
 import React, { useState, useRef, useEffect } from 'react'
 import { makeStyles } from '@mui/styles'
 import Container from '@mui/material/Container'
-
+import { fillCanvas } from './fillCanvas'
 import Menu from './Menu'
 import Header from './Header'
 
@@ -95,94 +91,6 @@ function Canvas({ socket }) {
     }
   }
 
-  /** get the color of the pixel */
-  const getPixelColor = (imageData, x, y) => {
-    if (x < 0 || y < 0 || x >= imageData.width || y >= imageData.height) {
-      return [-1, -1, -1, -1] // impossible color
-    }
-
-    const offset = (y * imageData.width + x) * 4
-    return imageData.data.slice(offset, offset + 4)
-  }
-
-  const compareColor = (color1, color2) => {
-    for (let i = 0; i < color2.length; i += 1) {
-      if (color1[i] !== color2[i]) return false
-    }
-    return true
-  }
-  /** draw the pixel */
-  const drawPixel = (imageData, x, y, color) => {
-    const offset = (y * imageData.width + x) * 4
-    imageData.data[offset + 0] = color[0]
-    imageData.data[offset + 1] = color[1]
-    imageData.data[offset + 2] = color[2]
-    imageData.data[offset + 3] = 255
-  }
-
-  /** convert hex color to rgb */
-  const hexToRgb = (h) => {
-    let r = 0
-    let g = 0
-    let b = 0
-
-    // 3 digits
-    if (h.length === 4) {
-      r = `0x${h[1]}${h[1]}`
-      g = `0x${h[2]}${h[2]}`
-      b = `0x${h[3]}${h[3]}`
-
-    // 6 digits
-    } else if (h.length === 7) {
-      r = `0x${h[1]}${h[2]}`
-      g = `0x${h[3]}${h[4]}`
-      b = `0x${h[5]}${h[6]}`
-    }
-
-    return [+r, +g, +b]
-  }
-
-  const fillCanvas = (drawObject) => {
-    const fillColor = hexToRgb(drawObject.fill.color)
-
-    // get the data of the current canvas
-    const imgData = ctx.getImageData(
-      0,
-      0,
-      canvasDim.width,
-      canvasDim.height,
-    )
-    // get the color targeted
-    const targetColor = getPixelColor(imgData, drawObject.x0, drawObject.y0)
-    if (!compareColor(targetColor, fillColor)) {
-      // recursivity don't work !
-      // creation of a stack whith the position
-      const stack = [drawObject.x0, drawObject.y0]
-
-      while (stack.length > 0) {
-        const y = stack.pop()
-        const x = stack.pop()
-
-        const currentColor = getPixelColor(imgData, x, y)
-        // the color targeted is the same as the current color pixel
-        if (compareColor(targetColor, currentColor)) {
-          // fill the pixel
-          drawPixel(imgData, x, y, fillColor)
-          // we check the four direction
-          stack.push(x + 1, y)
-          stack.push(x - 1, y)
-          stack.push(x, y + 1)
-          stack.push(x, y - 1)
-        }
-      }
-    }
-    // put the new data
-    ctx.putImageData(imgData, 0, 0)
-    if (socket && socket.id === drawObject.senderId) {
-      socket.emit('draw', drawObject)
-    }
-  }
-
   /** Init/Update values */
   useEffect(() => {
     const canvas = canvasRef.current
@@ -203,7 +111,7 @@ function Canvas({ socket }) {
           } else if (drawObject.eraser.isActive) {
             erase(drawObject)
           } else if (drawObject.fill.isActive) {
-            fillCanvas(drawObject)
+            fillCanvas(drawObject, ctx, canvasDim, socket)
           }
         }
       })
@@ -243,7 +151,7 @@ function Canvas({ socket }) {
         x0: pos.X,
         y0: pos.Y,
         senderId: socket.id,
-      })
+      }, ctx, canvasDim, socket)
     }
   }
 

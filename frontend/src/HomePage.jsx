0b40a4c3ea72220fc8a5745b88d80ab527df3677
Antoine <antoinechataignier1998@orange.fr>
2022-03-22 13:12:08 +0100

    feat: add header in home page

---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-named-as-default-member */
+import React from 'react'
+import Header from './components/header/Header'
+import './App.css'
+
+function HomePage() {
+  return (
+    <Header />
+  )
+}
+export default HomePage
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import clsx from 'clsx'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+  header: {
+    maxHeight: '300px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  pentaboy: {
+    height: '300px',
+    width: 'auto',
+    objectFit: 'contain',
+    marginRight: '-75px',
+    position: 'relative',
+    zIndex: '2',
+    transform: 'rotate(-8deg)',
+  },
+  title: {
+    textTransform: 'uppercase',
+    '-webkit-text-stroke-width': '2px',
+    '-webkit-text-stroke-color': 'black',
+    letterSpacing: '2px',
+    fontSize: '90px',
+
+  },
+})
+
+function Header() {
+  const classes = useStyles()
+  return (
+    <header className={classes.header}>
+      <img src="/pentaboy.svg" alt="pentaboy" className={classes.pentaboy} />
+      <h1 className={clsx(classes.title, 'letter-shadow')}>
+        Penta
+        <br />
+        Room
+      </h1>
+    </header>
+  )
+}
+export default Header

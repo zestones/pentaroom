9c4aca49da81677c82e546a7f2173e182c7f2879
Antoine <antoinechataignier1998@orange.fr>
2022-03-29 20:53:33 +0200

    refactor: add Canvas

---
+++ frontend/src/components/Canvas/Canvas.jsx
@@ -0,0 +1,324 @@
+import React, {
+  useState, useRef, useEffect, useContext,
+} from 'react'
+import { makeStyles } from '@mui/styles'
+import Container from '@mui/material/Container'
+import { fillCanvas } from './FillCanvas'
+import Menu from './Menu'
+import { SocketContext } from '../../context/socket'
+
+/** Canvas Styles */
+const useStyles = makeStyles({
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  drawArea: {
+    flexGrow: '1',
+    maxWidth: '100%',
+    position: 'relative',
+    backgroundColor: 'white',
+    marginTop: '5px',
+  },
+  icon: {
+    margin: '0 auto',
+    padding: '20px 40px',
+  },
+})
+
+function Canvas({ userRole }) {
+  const socket = useContext(SocketContext)
+
+  // Canvas
+  const canvasRef = useRef(null)
+
+  const [ctx, setCtx] = useState(null)
+
+  const [canvasDim, setCanvasDim] = useState({ width: 0, height: 0 })
+
+  const [isInAction, setIsInAction] = useState(false)
+
+  const myCanvas = document.getElementById('myCanvas')
+
+  const [userDraw, setUserDraw] = useState({
+    x0: 0,
+    y0: 0,
+    x1: 0,
+    y1: 0,
+    pen: {
+      isActive: true,
+      color: '#000000',
+      width: 10,
+    },
+    eraser: {
+      isActive: false,
+      width: 10,
+    },
+    fill: {
+      isActive: false,
+      color: '#000000',
+    },
+    clear: {
+      isActive: false,
+    },
+    undo: {
+      undoList: [],
+      isActive: false,
+    },
+    redo: {
+      redoList: [],
+      isActive: false,
+    },
+  })
+
+  const activeDefaultTool = () => {
+    ctx.strokeStyle = userDraw.pen.color
+    setUserDraw({
+      ...userDraw,
+      pen: { ...userDraw.pen, isActive: true },
+    })
+  }
+
+  /** Draw */
+  const draw = (drawObject) => {
+    ctx.beginPath()
+
+    ctx.moveTo(drawObject.x0, drawObject.y0)
+    ctx.lineTo(drawObject.x1, drawObject.y1)
+
+    ctx.strokeStyle = drawObject.pen.color
+    ctx.lineWidth = drawObject.pen.width
+    ctx.lineJoin = 'round'
+    ctx.lineCap = 'round'
+
+    ctx.stroke()
+    ctx.closePath()
+
+    if (socket && socket.id === drawObject.senderId) {
+      socket.emit('draw', drawObject)
+    }
+  }
+
+  /** Clean the screen with the eraser */
+  const erase = (drawObject) => {
+    ctx.beginPath()
+
+    ctx.moveTo(drawObject.x0, drawObject.y0)
+    ctx.lineTo(drawObject.x1, drawObject.y1)
+
+    ctx.strokeStyle = 'white'
+    ctx.lineWidth = drawObject.eraser.width
+
+    ctx.lineJoin = 'round'
+    ctx.lineCap = 'round'
+    ctx.stroke()
+    ctx.closePath()
+
+    if (socket && socket.id === drawObject.senderId) {
+      socket.emit('draw', drawObject)
+    }
+  }
+
+  const clear = (drawObject) => {
+    ctx.clearRect(0, 0, canvasDim.width, canvasDim.height)
+    if (socket && socket.id === drawObject.senderId) {
+      socket.emit('draw', drawObject)
+    }
+    activeDefaultTool()
+  }
+
+  /** save the canvas */
+  const saveCanvas = (keepRedoList, list) => {
+    if (!keepRedoList) {
+      userDraw.redo.redoList = []
+    }
+    (list || userDraw.undo.undoList).push(myCanvas.toDataURL())
+  }
+
+  /** restore the canvas */
+  // pop is the array which the img will be restored
+  // push is the array which a new img will be added
+  const restoreCanvas = (pop, push) => {
+    if (pop.length) {
+      // get the last image saved
+      const restore = pop.pop()
+      // save a new image
+      saveCanvas(true, push)
+
+      // Create img with the data in pop
+      const img = new Image()
+      img.src = restore
+
+      // clear the canvas
+      ctx.clearRect(0, 0, canvasDim.width, canvasDim.height)
+
+      // draw the previous canvas
+      img.onload = function reDraw() {
+        ctx.drawImage(
+          img,
+          0,
+          0,
+          canvasDim.width,
+          canvasDim.height,
+          0,
+          0,
+          canvasDim.width,
+          canvasDim.height,
+        )
+      }
+    }
+  }
+
+  /** get the previous canvas */
+  const undoCanvas = (drawObject) => {
+    if (socket && socket.id === drawObject.senderId) {
+      socket.emit('draw', drawObject)
+    }
+    restoreCanvas(drawObject.undo.undoList, drawObject.redo.redoList)
+    activeDefaultTool()
+  }
+
+  /** get the <next> canvas */
+  const redoCanvas = (drawObject) => {
+    if (socket && socket.id === drawObject.senderId) {
+      socket.emit('draw', drawObject)
+    }
+    restoreCanvas(drawObject.redo.redoList, drawObject.undo.undoList)
+    activeDefaultTool()
+  }
+
+  /** Init/Update values */
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+
+    context.lineJoin = 'round'
+    context.lineCap = 'round'
+
+    setCanvasDim({ width: document.getElementById('draw').offsetWidth, height: document.getElementById('draw').offsetHeight })
+
+    setCtx(context)
+
+    if (socket) {
+      socket.on('draw', (drawObject) => {
+        if (drawObject.senderId !== socket.id) {
+          if (drawObject.pen.isActive) {
+            draw(drawObject)
+          } else if (drawObject.eraser.isActive) {
+            erase(drawObject)
+          } else if (drawObject.fill.isActive) {
+            fillCanvas(drawObject, ctx, canvasDim, socket)
+          } else if (drawObject.clear.isActive) {
+            clear(drawObject)
+          } else if (drawObject.undo.isActive) {
+            undoCanvas(drawObject)
+          } else if (drawObject.redo.isActive) {
+            redoCanvas(drawObject)
+          }
+        }
+      })
+    }
+  }, [socket, setCtx, setCanvasDim])
+
+  /** get current position on the screen */
+  const getPositionOnEvent = (e) => {
+    let X
+    let Y
+
+    // handle touch Event and Mouse Event
+    if (window.TouchEvent && e.nativeEvent instanceof TouchEvent) {
+      const canv = e.target.getBoundingClientRect()
+      X = e.targetTouches[0].pageX - canv.left
+      Y = e.targetTouches[0].pageY - canv.top
+    } else {
+      X = e.nativeEvent.offsetX
+      Y = e.nativeEvent.offsetY
+    }
+    return { X, Y }
+  }
+
+  /** Start the drawing */
+  const handleTouchStart = (e) => {
+    if (userRole === 'server') return
+    setIsInAction(true)
+
+    saveCanvas(false)
+
+    const pos = getPositionOnEvent(e)
+    setUserDraw({
+      ...userDraw, x0: pos.X, x1: pos.X, y0: pos.Y, y1: pos.Y,
+    })
+    if (userDraw.fill.isActive) {
+      setUserDraw({
+        ...userDraw, color: ctx.strokeStyle,
+      })
+      fillCanvas({
+        ...userDraw,
+        x0: pos.X,
+        y0: pos.Y,
+        senderId: socket.id,
+      }, ctx, canvasDim, socket)
+    }
+  }
+
+  /** handle the mouse mouvement */
+  const handleTouchMove = (e) => {
+    if (!isInAction) return
+    if (userDraw.pen.isActive) {
+      const pos = getPositionOnEvent(e)
+      setUserDraw({
+        ...userDraw, x0: userDraw.x1, y0: userDraw.y1, x1: pos.X, y1: pos.Y,
+      })
+      draw({ ...userDraw, senderId: socket.id })
+    } else if (userDraw.eraser.isActive) {
+      const pos = getPositionOnEvent(e)
+      setUserDraw({
+        ...userDraw, x0: userDraw.x1, y0: userDraw.y1, x1: pos.X, y1: pos.Y,
+      })
+      erase({ ...userDraw, senderId: socket.id })
+    }
+  }
+
+  const handleTouchEnd = () => {
+    setIsInAction(false)
+  }
+
+  const classes = useStyles()
+
+  return (
+    <Container maxWidth="xl" className={classes.main}>
+      <div id="draw" className={classes.drawArea}>
+        <canvas
+          id="myCanvas"
+          onTouchStart={handleTouchStart}
+          onMouseDown={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onMouseMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+          onMouseUp={handleTouchEnd}
+          ref={canvasRef}
+          width={canvasDim.width}
+          height={canvasDim.height}
+        />
+      </div>
+      {(userRole !== 'server')
+        ? (
+          <Menu
+            userDraw={userDraw}
+            setUserDraw={setUserDraw}
+            setIsInAction={setIsInAction}
+            clear={clear}
+            socket={socket}
+            undoCanvas={undoCanvas}
+            redoCanvas={redoCanvas}
+          />
+        )
+        : (
+          <div className={classes.icon} />
+        )}
+    </Container>
+  )
+}
+
+export default Canvas
---
+++ frontend/src/components/Canvas/Menu.jsx
@@ -0,0 +1,211 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@mui/styles'
+import Container from '@mui/material/Container'
+import BrushIcon from '@mui/icons-material/Brush'
+import AutoFixNormalIcon from '@mui/icons-material/AutoFixNormal'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
+import Chip from '@mui/material/Chip'
+import Zoom from '@mui/material/Zoom'
+import Box from '@mui/material/Box'
+import FormatColorFillIcon from '@mui/icons-material/FormatColorFill'
+import RedoIcon from '@mui/icons-material/Redo'
+import UndoIcon from '@mui/icons-material/Undo'
+import IconButton from '@mui/material/IconButton'
+import clsx from 'clsx'
+
+const useStyles = makeStyles({
+  menuBar: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderRadius: '5px',
+    alignItems: 'center',
+    margin: '5px auto 30px auto',
+  },
+  chipsContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: '5px',
+    padding: '15px 30px',
+    height: '35px',
+    '&.active': {
+      backgroundColor: 'purple',
+    },
+  },
+  tools: {
+    marginTop: '15px',
+    height: '50px',
+  },
+})
+
+function Menu(props) {
+  const classes = useStyles()
+  const {
+    userDraw, setUserDraw, setIsInAction, clear, socket, undoCanvas, redoCanvas,
+  } = props
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null)
+  }
+  const open = Boolean(anchorEl)
+
+  const activePen = () => {
+    setUserDraw({
+      ...userDraw,
+      pen: { ...userDraw.pen, isActive: true },
+      eraser: { ...userDraw.eraser, isActive: false },
+      fill: { ...userDraw.fill, isActive: false },
+      clear: { ...userDraw.clear, isActive: false },
+      undo: { ...userDraw.undo, isActive: false },
+      redo: { ...userDraw.redo, isActive: false },
+    })
+    setIsInAction(false)
+  }
+  const activeEraser = () => {
+    setUserDraw({
+      ...userDraw,
+      pen: { ...userDraw.pen, isActive: false },
+      eraser: { ...userDraw.eraser, isActive: true },
+      fill: { ...userDraw.fill, isActive: false },
+      clear: { ...userDraw.clear, isActive: false },
+      undo: { ...userDraw.undo, isActive: false },
+      redo: { ...userDraw.redo, isActive: false },
+    })
+    setIsInAction(false)
+  }
+  const activeFill = () => {
+    setUserDraw({
+      ...userDraw,
+      pen: { ...userDraw.pen, isActive: false },
+      eraser: { ...userDraw.eraser, isActive: false },
+      fill: { ...userDraw.fill, isActive: true },
+      clear: { ...userDraw.clear, isActive: false },
+      undo: { ...userDraw.undo, isActive: false },
+      redo: { ...userDraw.redo, isActive: false },
+    })
+    setIsInAction(false)
+  }
+
+  const activeClear = () => {
+    /** set Dont work ??!! */
+    userDraw.pen.isActive = false
+    userDraw.fill.isActive = false
+    userDraw.eraser.isActive = false
+    userDraw.clear.isActive = true
+    userDraw.undo.isActive = false
+    userDraw.redo.isActive = false
+    userDraw.redo.redoList = []
+    userDraw.undo.undoList = []
+    clear({ ...userDraw, senderId: socket.id })
+  }
+
+  const activeUndo = () => {
+    userDraw.pen.isActive = false
+    userDraw.fill.isActive = false
+    userDraw.eraser.isActive = false
+    userDraw.redo.isActive = false
+    userDraw.undo.isActive = true
+
+    undoCanvas({ ...userDraw, senderId: socket.id })
+  }
+  const activeRedo = () => {
+    userDraw.pen.isActive = false
+    userDraw.fill.isActive = false
+    userDraw.eraser.isActive = false
+    userDraw.redo.isActive = false
+    userDraw.undo.isActive = false
+    userDraw.redo.isActive = true
+
+    redoCanvas({ ...userDraw, senderId: socket.id })
+  }
+
+  const checked = userDraw.pen.isActive || userDraw.eraser.isActive
+   || userDraw.fill.isActive
+  return (
+    <Container
+      maxWidth="sm"
+      className={classes.menuBar}
+      aria-owns={open ? 'tool-popover' : undefined}
+      aria-haspopup="true"
+      onMouseEnter={handlePopoverOpen}
+      onMouseLeave={handlePopoverClose}
+    >
+      <Box className={classes.chipsContainer}>
+        <Chip className={clsx(classes.chip, userDraw.pen.isActive && 'active')} color="primary" icon={<BrushIcon />} label="Pinceau" onClick={() => { activePen() }} />
+        <Chip className={clsx(classes.chip, userDraw.eraser.isActive && 'active')} color="primary" icon={<AutoFixNormalIcon />} label="Gomme" onClick={() => { activeEraser() }} />
+        <Chip className={clsx(classes.chip, userDraw.fill.isActive && 'active')} color="primary" icon={<FormatColorFillIcon />} label="Remplissage" onClick={() => { activeFill() }} />
+        <Chip className={classes.chip} color="primary" icon={<HighlightOffIcon />} label="Effacer tout" onClick={() => { activeClear() }} />
+        <IconButton className={classes.undo} color="primary" aria-label="undo action" component="span" onClick={() => activeUndo()}><UndoIcon /></IconButton>
+        <IconButton className={classes.redo} color="primary" aria-label="redo action" component="span" onClick={() => activeRedo()}><RedoIcon /></IconButton>
+      </Box>
+      <Box className={classes.tools}>
+        <Zoom in={checked} style={{ transitionDelay: checked ? '500ms' : '0ms' }}>
+          <Box>
+            {userDraw.pen.isActive && (
+              <>
+                <input
+                  type="color"
+                  value={userDraw.pen.color}
+                  onChange={(e) => setUserDraw({
+                    ...userDraw,
+                    pen: { ...userDraw.pen, color: e.target.value },
+                  })}
+
+                />
+                <input
+                  type="range"
+                  min="0"
+                  max="50"
+                  value={userDraw.pen.width}
+                  onChange={(e) => setUserDraw({
+                    ...userDraw,
+                    pen: { ...userDraw.pen, width: e.target.value },
+                  })}
+
+                />
+              </>
+            )}
+
+            {userDraw.eraser.isActive && (
+              <input
+                type="range"
+                min="10"
+                max="100"
+                value={userDraw.eraser.width}
+                onChange={(e) => setUserDraw({
+                  ...userDraw,
+                  eraser: { ...userDraw.eraser, width: e.target.value },
+                })}
+
+              />
+            )}
+
+            {userDraw.fill.isActive && (
+              <input
+                type="color"
+                value={userDraw.fill.color}
+                onChange={(e) => setUserDraw({
+                  ...userDraw,
+                  fill: { ...userDraw.fill, color: e.target.value },
+                })}
+              />
+            )}
+
+          </Box>
+
+        </Zoom>
+      </Box>
+
+    </Container>
+  )
+}
+export default Menu

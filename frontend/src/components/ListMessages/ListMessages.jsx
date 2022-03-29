d0076b36cf86c65ea4dfaff1ff96a5d5c8a442f5
islem711 <75393150+islem711@users.noreply.github.com>
2022-03-29 23:54:11 +0200

    refactor: style ListMessages en scss

--- frontend/src/components/ListMessages/ListMessages.jsx
+++ frontend/src/components/ListMessages/ListMessages.jsx
@@ -1,37 +1,14 @@
 import React, {
   useRef, useState, useEffect, useContext,
 } from 'react'
-import { makeStyles } from '@mui/styles'
 import Paper from '@mui/material/Paper'
 import Message from './Message'
 import { SocketContext } from '../../context/socket'
+import './ListMessages.scss'
 
-const useStyles = makeStyles({
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#263238',
-    flexGrow: '1',
-  },
-  paper: {
-    width: '100%',
-    height: '100%',
-    position: 'relative',
-    borderRadius: 0,
-  },
-  messageContainer: {
-    overflowY: 'auto',
-    height: '85%',
-  },
-  ol: {
-    paddingInlineEnd: '40px',
-  },
-})
 function ListMessages() {
   const socket = useContext(SocketContext)
   const [messages, setMessages] = useState([])
-  const classes = useStyles()
   const messageRef = useRef()
 
   const handleNewMessages = (newReceivedMessage) => {
@@ -47,10 +24,10 @@ function ListMessages() {
   }, [socket, messages])
 
   return (
-    <div className={classes.container}>
-      <Paper elevation={5} className={classes.paper}>
-        <div className={classes.messageContainer}>
-          <ol className={classes.ol}>
+    <div className="container">
+      <Paper elevation={5} className="paper">
+        <div className="messageContainer">
+          <ol className="ol">
             {messages.map((message) => (
               <Message key={message.id} message={message} />
             ))}

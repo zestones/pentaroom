bc95a6fca2f31ec8e8f094c3b13ab1f0f587110c
Idriss <idrissbenguezzou@gmail.com>
2022-03-30 01:30:24 +0200

    refactor(UserInput): refactor styles

--- frontend/src/components/UserInput/UserInput.jsx
+++ frontend/src/components/UserInput/UserInput.jsx
@@ -1,63 +1,12 @@
 import React, { useRef } from 'react'
+import './UserInput.scss'
 import Container from '@mui/material/Container'
 import Button from '@mui/material/Button'
 import SendIcon from '@mui/icons-material/Send'
-import { makeStyles } from '@mui/styles'
 import OutlinedInput from '@mui/material/OutlinedInput'
 import TransparentContainer from '../TransparentContainer/TransparentContainer'
 
-const useStyles = makeStyles({
-  superContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100%',
-    padding: '30px 0',
-  },
-  container: {
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '0',
-  },
-  inputContainer: {
-    margin: '20px auto',
-  },
-  subcontainer: {
-    textAlign: 'center',
-  },
-  sendButton: {
-    marginTop: '30px',
-  },
-  hiddenWord: {
-    color: 'black',
-    textAlign: 'center',
-  },
-  scores: {
-    position: 'absolute',
-    bottom: '50px',
-    left: '300px',
-  },
-  inputWord: {
-    borderRadius: '50px',
-    backgroundColor: '#cce3f6',
-    border: '1px solid black',
-    padding: '0px 15px',
-    fontSize: '20px',
-  },
-  title: {
-    fontSize: '30px',
-    margin: '0',
-    marginBottom: '30px',
-    '-webkit-text-stroke-width': '2px',
-    '-webkit-text-stroke-color': 'black',
-    letterSpacing: '1px',
-  },
-})
-
 function UserInput() {
-  const classes = useStyles()
-
   const inputRef = useRef('')
 
   const handleValidation = () => {
@@ -72,20 +21,20 @@ function UserInput() {
   }
 
   return (
-    <Container maxWidth="xxl" className={classes.superContainer}>
-      <Container className={classes.subcontainer} maxWidth="lg">
-        <div className={classes.hiddenWord} />
-        <TransparentContainer backgroundColor="#0000A5" className={classes.inputContainer}>
-          <h2 className={classes.title}>Entre un mot : </h2>
+    <Container maxWidth="xxl" className="super-container">
+      <Container className="subcontainer" maxWidth="lg">
+        <div className="hidden-word" />
+        <TransparentContainer backgroundColor="#0000A5" className="input-container">
+          <h2 className="title">Entre un mot : </h2>
           <OutlinedInput
             inputRef={inputRef}
             fullWidth
             placeholder="Tape le mot ici ..."
-            className={classes.inputWord}
+            className="input-word"
             onKeyPress={handleKeyPressed}
           />
         </TransparentContainer>
-        <Button className={classes.sendButton} variant="contained" endIcon={<SendIcon />} onClick={handleValidation}>
+        <Button className="send-btn" variant="contained" endIcon={<SendIcon />} onClick={handleValidation}>
           Envoyer
         </Button>
 

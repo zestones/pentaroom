d3fa4ab2dc05b79c469a2d5aa2cc7af5dbf9f691
mohamedlrouabah <mohamedlrouabah@gmail.com>
2022-03-29 23:22:29 +0200

    add : scss for playButton

--- frontend/src/components/PlayButton/PlayButton.jsx
+++ frontend/src/components/PlayButton/PlayButton.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import Button from '@mui/material/Button'
-import { makeStyles } from '@mui/styles'
-
-const useStyles = makeStyles({
-  playButton: {
-    margin: '0 auto',
-    display: 'block',
-    fontSize: '35px',
-    lineHeight: '1',
-    padding: '20px 40px',
-    borderRadius: '50px',
-    backgroundColor: '#f58322',
-    fontFamily: 'inherit',
-  },
-})
+import './PlayButton.scss'
 
 function PlayButton({ onClick }) {
-  const classes = useStyles()
   return (
-    <Button onClick={onClick} variant="contained" className={classes.playButton}>C&lsquo;est Parti !</Button>
+    <Button onClick={onClick} variant="contained" className="playButton">C&lsquo;est Parti !</Button>
   )
 }
 export default PlayButton

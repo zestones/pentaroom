57995309b41a2eb906e76207506a2a31ba5904f8
Idriss <idrissbenguezzou@gmail.com>
2022-04-04 01:26:54 +0200

    style(ListUsers): add style to user drawer

--- frontend/src/components/ListUsers/ListUsers.jsx
+++ frontend/src/components/ListUsers/ListUsers.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useContext, useEffect } from 'react'
 import Avatar from 'react-nice-avatar'
-import { SocketContext } from '../../context/socket'
+import clsx from 'clsx'
 import styles from './ListUsers.module.scss'
+
+import { SocketContext } from '../../context/socket'
 
 function ListUsers({ title, order = false }) {
   const socket = useContext(SocketContext)
@@ -48,9 +50,15 @@ function ListUsers({ title, order = false }) {
         {users.map((user) => (
           (isRegister(user))
             && (
-              <li key={user.id} className={styles.userContainer}>
+              <li
+                key={user.id}
+                className={
+                  clsx(styles.userContainer, (drawerId === user.id) && styles.userDrawer)
+                }
+              >
                 <Avatar fontSize="medium" className={styles.avatar} {...user.avatar} />
                 <p className={styles.pseudo} key={user.id}>{user.pseudo}</p>
+                {drawerId === user.id && <img src="/pentaboy.svg" alt="pentaboy" className={styles.pentaboy} />}
                 <p className={styles.score}>{user.score}</p>
               </li>
             )
